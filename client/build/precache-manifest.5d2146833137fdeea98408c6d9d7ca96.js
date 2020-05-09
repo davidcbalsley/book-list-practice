113d647dbcb1f75a9115e0148cae5118cf175d6e
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-05-09 14:40:26 -0500

    initial commit

---
+++ client/build/precache-manifest.5d2146833137fdeea98408c6d9d7ca96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b0801183b3081f4ad3",
+    "url": "/static/css/main.559e6517.chunk.css"
+  },
+  {
+    "revision": "36b0801183b3081f4ad3",
+    "url": "/static/js/main.c5d27a6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0d5c17320b3994529e0",
+    "url": "/static/js/2.998b6ee3.chunk.js"
+  },
+  {
+    "revision": "da103d1b001d47c314bdf07963b4c241",
+    "url": "/index.html"
+  }
+];
